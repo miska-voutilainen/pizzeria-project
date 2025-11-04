fea673ab2e47c1ce370e16eed688bae9ccf71c44
taif <taifjalo0@gmail.com>
2025-11-04 21:57:46 +0200

    trying to fix: rename FetchApi.js to fetchApi.js

--- @server/index.js
+++ @server/index.js
@@ -14,6 +14,7 @@ const db = await connectMongoDB();
 
 app.use("/api", createAuthRoutes(db));
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT || 3001}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
