47053d94d1cc8035b6e0d6719f4d6fb4ed59cc25
taif <taifjalo0@gmail.com>
2025-12-02 12:56:53 +0200

    Add Frontend and Backend setup.

--- @server/index.js
+++ @server/index.js
@@ -1,19 +1,27 @@
-import express from 'express';
-import { connectMariaDB, closeMariaDB } from './config/db.js';
-import createAuthRoutes from './src/routes/authRoutes.js';
+import express from "express";
+import cors from "cors";
+import { connectMariaDB, closeMariaDB } from "./config/db.js";
+import createAuthRoutes from "./src/routes/authRoutes.js";
+import foodRoutes from "./src/routes/foodRoutes.js";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.development" });
 
 (async () => {
   const pool = await connectMariaDB();
   const app = express();
+  app.use(cors());
 
   app.use(express.json());
-  app.use('/api', createAuthRoutes(pool));
+  app.use("/api", createAuthRoutes(pool));
+  app.use("/", foodRoutes(pool));
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`API listening on ${PORT}`));
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+  );
 
   // graceful shutdown
-  process.on('SIGINT', async () => {
+  process.on("SIGINT", async () => {
     await closeMariaDB();
     process.exit(0);
   });
